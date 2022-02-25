45e68eeb2d8e0fa3d1c412f9b9d23a80df9eec86
Filip Johnsson <Filip.johnsson1989@gmail.com>
2022-02-25 09:39:48 +0100

    Add Entities

---
+++ Lms.Core/Entities/Course.cs
@@ -0,0 +1,9 @@
+﻿namespace Lms.Core.Entities;
+
+public class Course
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public DateTime StartDate { get; set; }
+    public ICollection<Module> Modules { get; set; }
+}
---
+++ Lms.Core/Entities/Module.cs
@@ -0,0 +1,9 @@
+﻿namespace Lms.Core.Entities;
+
+public class Module
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public DateTime StartDate { get; set; }
+    public int CourseId { get; set; }
+}
