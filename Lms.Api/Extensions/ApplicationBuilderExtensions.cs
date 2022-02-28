8a5fcc3b65c7844277d813817946b74c6c063c93
Filip Johnsson <Filip.johnsson1989@gmail.com>
2022-02-28 11:08:38 +0100

    Exception handler for Seed Data

--- Lms.Api/Extensions/ApplicationBuilderExtensions.cs
+++ Lms.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -6,18 +6,20 @@ public static class ApplicationBuilderExtensions
 {
     public static async Task<IApplicationBuilder> SeedDataAsync(this IApplicationBuilder app)
     {
-        using(var scope= app.ApplicationServices.CreateScope())
+        using (var scope = app.ApplicationServices.CreateScope())
         {
-            var serviceProvider= scope.ServiceProvider;
-            //try
-            //{
+            var serviceProvider = scope.ServiceProvider;
+            try
+            {
                 var LmsDbContext = serviceProvider.GetRequiredService<LmsDbContext>();
                 await SeedData.InitAsync(LmsDbContext);
-            //}
-            //catch(Exception ex)
-            //{
-
-            //}
+            }
+            catch (Exception ex)
+            {
+                var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+                logger.LogError(string.Join(" ", ex.Message));
+                //throw ex;
+            }
 
         }
 
