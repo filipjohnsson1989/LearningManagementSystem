4b3033b12a228350d7bd4e2573568d556de17b64
Filip Johnsson <Filip.johnsson1989@gmail.com>
2022-02-28 11:05:17 +0100

    Bring Course Instead Of CourseID In Module

--- Lms.Core/Entities/Module.cs
+++ Lms.Core/Entities/Module.cs
@@ -5,5 +5,5 @@ public class Module
     public int Id { get; set; }
     public string Title { get; set; } = null!;
     public DateTime StartDate { get; set; }
-    public int CourseId { get; set; }
+    public Course Course { get; set; } = null!;
 }
